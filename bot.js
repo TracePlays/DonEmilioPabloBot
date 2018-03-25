9c6071253bac7ec4effd5ad1bd079f4d1a5ba154
TracePlays <37755568+TracePlays@users.noreply.github.com>
2018-03-25 13:26:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,11 +11,30 @@ client.on('message', message => {
   	}
 });
 
-client.on('message', message => {
-    if (message.content === 'bing') {
-    	message.reply('BONG!');
-  	}
+//client.on('message', message => {
+  //  if (message.content === 'bing') {
+   // 	message.reply('BONG!');
+  	//}
+//});
+client.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+       
+        args = args.splice(1);
+        switch(cmd) {
+            // !ping
+            case 'Don':
+                client.sendMessage({
+                    to: channelID,
+                    message: 'Wau!'
+                });
+            break;
+            // Just add any case commands if you want to..
+         }
+     }
 });
-
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
