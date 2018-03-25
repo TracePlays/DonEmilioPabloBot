5f2ad52221dd93aa626245eac6d1942a6b862086
TracePlays <37755568+TracePlays@users.noreply.github.com>
2018-03-25 14:10:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = '!';
 
 client.on('ready', () => {
     console.log('I am ready!');
@@ -16,22 +17,15 @@ client.on('message', message => {
    // 	message.reply('BONG!');
   	//}
 //});
-client.on('message', message => {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'don':
-               message.channel.send('Wau!');
+client.on('message', function(message) {
+    if(!message.content.startsWith(prefix)) return;
+    
+    var args = message.content.substring(prefix.length).split(" ");
+    
+    switch(args[0]){
+        case 'ping': 
+            message.channel.sendMessage('Pong!');
             break;
-            // Just add any case commands if you want to..
-         }
-     }
 });
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
