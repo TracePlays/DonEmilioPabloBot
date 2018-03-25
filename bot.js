9b61060a08176eb36b558a178a2be10364431fac
TracePlays <37755568+TracePlays@users.noreply.github.com>
2018-03-25 14:24:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,31 +2,19 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const prefix = '!';
 
-//var bot = new Discord.Client();
 
 bot.on('ready', function() {
     console.log('ready!');
 });
 
-//client.on('message', message => {
-  //  if (message.content === 'ping') {
-    //	message.channel.send('PONG!');
-  	//}
-//});
-
-//client.on('message', message => {
-  //  if (message.content === 'bing') {
-   // 	message.reply('BONG!');
-  	//}
-//});
 bot.on('message', function(message) {
     if(!message.content.startsWith(prefix)) return;
-    
+    //nach prefix content splitten
     var args = message.content.substring(prefix.length).split(" ");
     
     switch(args[0]){
-        case 'ping': 
-            message.channel.sendMessage('Pong!');
+        case 'don': 
+            message.channel.sendMessage('Wau!');
             break;
     }
 });
